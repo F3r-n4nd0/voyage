eb58bbfab9dd9bf92243f54ff9fc5aa9412c6aca
Fernando Luna <337n4nd0@gmail.com>
2020-06-04 20:04:14 -0400

    Initial Commit

---
+++ voyage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  voyage
+//
+//  Created by Fernando Luna on 6/4/20.
+//  Copyright © 2020 Fernando Luna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
